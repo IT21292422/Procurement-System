5c1e3c1f5945237690b6dc113b6ffdefade64ecd
it21294198 <it21294198@my.sliit.lk>
2023-10-10 13:10:37 +0530

    updated jest test

--- App.test.js
+++ App.test.js
@@ -1,15 +1,29 @@
-import * as React from 'react';
-import renderer from 'react-test-renderer';
+import * as React from "react";
+import { render, fireEvent } from "@testing-library/react-native";
+import ItemAdd, { addItem } from "./src/screens/ProcurementStaff/ItemAdd";
+import App from "./App";
 
-import App from './App';
+// describe('<App />', () => {
+//   it('has 1 child', () => {
+//     const tree = renderer.create(<App />).toJSON();
+//     if (tree) {
+//       expect(tree.children?.length).toBe(1);
+//     } else {
+//       throw new Error('Tree is null');
+//     }
+//   });
+// });
 
-describe('<App />', () => {
-  it('has 1 child', () => {
-    const tree = renderer.create(<App />).toJSON();
-    if (tree) {
-      expect(tree.children?.length).toBe(1);
-    } else {
-      throw new Error('Tree is null');
-    }
-  });
+test("addItem function works correctly", () => {
+  // Call the addItem function with an input value
+  const result = addItem(5);
+
+  // Assert the result of addItem function
+  expect(result).toBe(50); // Change this to the expected result
+});
+
+test("ItemAdd component renders correctly", () => {
+  const { getByText } = render(<ItemAdd />);
+  const itemAddText = getByText("Text");
+  expect(itemAddText).toBeDefined();
 });
--- src/screens/ProcurementStaff/ItemAdd.tsx
+++ src/screens/ProcurementStaff/ItemAdd.tsx
@@ -1,10 +1,18 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import React,{useEffect,useState} from 'react'
 
 export default function ItemAdd() {
+  const [userData, setUserData] = useState<number>(2);
+
+  useEffect(() => {
+    const val = 4;
+    const setVal = () => {
+      setUserData(val);
+    };
+    setVal();
+  }, []);
+
   return (
-    <View>
-      <Text>ItemAdd</Text>
-    </View>
-  )
+    <View><Text>Text</Text></View>
+  );
 }
