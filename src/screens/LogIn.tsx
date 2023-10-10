fbcbe5ddf6785a5e8e7024ea5a640da6ce44aca6
it21294198 <it21294198@my.sliit.lk>
2023-10-10 11:47:31 +0530

    add net info

--- src/screens/LogIn.tsx
+++ src/screens/LogIn.tsx
@@ -1,8 +1,15 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import { t } from 'react-native-tailwindcss';
+import NetInfo from '@react-native-community/netinfo';
 
 export default function LogIn() {
+
+  NetInfo.fetch().then(state => {
+  console.log('Connection type', state.type);
+  console.log('Is connected?', state.isConnected);
+  });
+
   return (
     <View style={[t.flexGrow,t.justifyAround,t.selfStart]}>
       <Text>LogIn</Text>
