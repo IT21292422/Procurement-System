381e4cd9e4c078f4c3b08505d3c8e3779b65e0d5
chamath-shanaka <chamathshanaka19@gmail.com>
2023-10-12 10:35:38 +0530

    getItemDetails()

--- src/screens/SiteManager/ItemDetails.tsx
+++ src/screens/SiteManager/ItemDetails.tsx
@@ -1,10 +1,47 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
+import { doc, getDoc } from "firebase/firestore";
+import { fireStore } from '../../../config/firebase';
+
+type ItemType = {
+	itemId: string;
+	itemName: string;
+	description: string;
+	unitPrice: number;
+	policy: string;
+}
 
 export default function ItemDetails() {
-  return (
-    <View>
-      <Text>ItemDetails</Text>
-    </View>
-  )
+	const itemId = 'cp7RzpuvXMxFUbCmctNM';
+	const [itemDetails, setItemDetails] = useState<ItemType>({
+		itemId: "",
+		itemName: "",
+		description: "",
+		unitPrice: 0,
+		policy: ""
+	});
+
+	// read item details
+	const getItemDetails = async () => {
+		try {
+			const docRef = doc(fireStore, 'items', itemId);
+			const docSnap = await getDoc(docRef);
+
+			if (docSnap.exists()) {
+				const data = docSnap.data() as ItemType;
+				setItemDetails(data);
+			} else {
+				console.log("No such document!");
+			}
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
+
+	return (
+		<View>
+			<Text>ItemDetails</Text>
+		</View>
+	)
 }   
