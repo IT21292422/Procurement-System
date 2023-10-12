6321f21e7e42938b5c95428aced6714ce9a0d8a0
Chamath Shanaka Wickrama Arachchi <81370105+chamath-shanaka@users.noreply.github.com>
2023-10-12 20:16:59 +0530

    OrderType revisions

--- config/types.ts
+++ config/types.ts
@@ -1,35 +1,33 @@
 import { Timestamp } from 'firebase/firestore';
 
 export type UserType = {
-	username:	 string,
-	userType:	 string, // site_manager | manager | procurement | supplier 
+	username:  string,
+	userType:  string, // site_manager | manager | procurement | supplier 
 	userEmail: string
 }
 
 export type ItemType = {
-	itemId: 		 string;
-	itemName:		 string;
+	itemId:      string;
+	itemName:    string;
 	description: string;
-	unitPrice: 	 number;
-	policy:			 string;
+	unitPrice:   number;
+	policy:      string;
 }
 
 export type OrderType = {
-	orderId: 			string,
-	isDraft: 			boolean,
-	itemId: 			string,
-	itemName: 		string,
-	quantity: 		number,
-	orderTotal: 	number,
+	orderId:      string,
+	isDraft:      boolean,
+	itemList:     Array<{ itemName: string; unitPrice: number; quantity: number }>,
+	orderTotal:   number,
 	deliverySite: string, // site name
-	status: 			string, // approval_pending | approved | delivery_pending | delivered ...
-	createdAt: 		Timestamp,
+	status:       string, // approval_pending | approved | delivery_pending | delivered ...
+	createdAt:    Timestamp,
 	purchaseDate: Timestamp,
-	supplierId: 	string
+	supplierName: string
 }
 
 export type SiteType = {
-	siteId: 	string,
+	siteId:   string,
 	siteName: string,
-	address: 	string
+	address:  string
 }
