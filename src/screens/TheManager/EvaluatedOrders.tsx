44adc72bfc5b33074a5bc0f2d121d1f15ae6f59e
IT21292422 <it21292422@my.sliit.lk>
2023-10-16 23:16:29 +0530

    fix issue

--- src/screens/TheManager/EvaluatedOrders.tsx
+++ src/screens/TheManager/EvaluatedOrders.tsx
@@ -36,9 +36,12 @@ export default function EvaluatedOrders() {
     useEffect(() => {
       receiveData()
     }, [orders])
+
+    //This will filter the order to make sure only the approved orders are rendered
+    const evaluatedOrders = orders.filter((order) => order.data.status === 'approved');
   
     //Iterator design pattern is used here to traverse through the array
-    const renderOrder = orders.map((order, index) => {
+    const renderOrder = evaluatedOrders.map((order, index) => {
   
       let btncolor: string = "blue"
       //This sets the style of the Order status based on the status
@@ -52,8 +55,6 @@ export default function EvaluatedOrders() {
         btncolor = "#17A2B8"
       }
       
-      //To filter only the orders with status approved
-      if (order.data.status === 'approved') {
         //Iterator design pattern is used here to traverse through the array
         const renderItem: any = (order.data.itemList || []).map((item: any) => {
           return (
@@ -116,7 +117,6 @@ export default function EvaluatedOrders() {
               <Button disabled={order.data.status === 'approved' || order.data.status === 'delivery_pending' || order.data.status === 'delivered'} onPress={() => showDialog(order.id)}>Authorize</Button>
             </Card.Actions>
           </Card>)
-      }
     })
   
     //This function is to change the status of an order to approved
@@ -127,13 +127,19 @@ export default function EvaluatedOrders() {
       }
       hideDialog()
     }
+
+    const noApproved = (
+      <View style={{alignItems:'center',marginTop:outerHeight/2-150}}>
+      <Text variant="headlineLarge" style={{color:"#28A745"}}>No approved orders found</Text>
+      </View>
+    );
   
   
    return (
       <>
         <ScrollView style={styles.scrollview}>
           <View style={styles.container}>
-            {renderOrder}
+          {evaluatedOrders.length === 0 ? noApproved : renderOrder}
           </View>
         </ScrollView>
         <Portal>
